851313c3ca3b999fd5dd271af1793c8bb212a8e5
Sujal Ranjan <90925047+Sranjan0208@users.noreply.github.com>
2023-10-16 11:44:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ document.addEventListener("DOMContentLoaded", function () {
   let count = 0;
   const countElement = document.getElementById("count");
 
-  // Function to update the count element
+
   function updateCount() {
     countElement.innerText = count;
   }
 
-  // Increment the counter
+
   document.getElementById("increment").addEventListener("click", function () {
     count;
     updateCount();
   });
 
-  // Decrement the counter (with a check to prevent negative count)
+
   document.getElementById("decrement").addEventListener("click", function () {
     count;
     updateCount();
